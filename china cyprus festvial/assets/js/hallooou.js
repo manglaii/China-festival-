bc60a25c4f3a18808deea887765c506716bdf818
Taotao Pan <pantaotao@icloud.com>
2017-06-15 11:35:58 +0300

    modify counter

--- china cyprus festvial/assets/js/hallooou.js
+++ china cyprus festvial/assets/js/hallooou.js
@@ -157,14 +157,14 @@ $(function() {
 // Counter
 $(function() {
 
-    // var eventHappenDay = new Date(2017, 8, 15, 12);
-    // var today = new Date();
-    // var difference = Math.abs(eventHappenDay - today);
-    // var days = difference / 36e5 / 24;
-    // var hours = (difference / 36e5) % 12;
+    var eventHappenDay = new Date(2017, 8, 15, 12);
+    var today = new Date();
+    var difference = Math.abs(eventHappenDay - today);
+    var days = difference / 36e5 / 24;
+    var hours = (difference / 36e5) % 12;
 
-    // $('#days.timer').text(parseInt(days));
-    // $('#hours.timer').text(parseInt(hours));
+    $('#days.timer').text(parseInt(days));
+    $('#hours.timer').text(parseInt(hours));
     $('.counter-section').on('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
             $(this).find('.timer').each(function() {
@@ -172,13 +172,10 @@ $(function() {
                 $({
                     Counter: 0
                 }).animate({
-                    Counter: $this.text()
+                    Counter: $(this).text()
                 }, {
                     duration: 2000,
                     easing: 'swing',
-                    step: function() {
-                        $this.text(Math.ceil(this.Counter));
-                    }
                 });
             });
             $(this).off('inview');
