f49a874fe43f3e6776d7ef1f641831c8e1771696
Taotao Pan <pantaotao@icloud.com>
2017-06-15 10:58:16 +0300

    change background image and modify counter

--- china cyprus festvial/assets/js/hallooou.js
+++ china cyprus festvial/assets/js/hallooou.js
@@ -157,6 +157,15 @@ $(function() {
 // Counter
 $(function() {
 
+    var eventHappenDay = new Date(2017, 8, 15, 12);
+    console.log(eventHappenDay);
+    var today = new Date();
+    var difference = Math.abs(eventHappenDay - today);
+    var days = difference / 36e5 / 24;
+    var hours = (difference / 36e5) % 12;
+
+    $('#days.timer').text(parseInt(days));
+    $('#hours.timer').text(parseInt(hours));
     $('.counter-section').on('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
             $(this).find('.timer').each(function() {
